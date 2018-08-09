1259fff31f1df7eef07d060732766e9c0739eec8
Daniel Elsner <dvelsner@gmail.com>
2018-08-09 21:58:33 +0200

    refactor protos

--- proto/compile_proto.sh
+++ proto/compile_proto.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 protoc --go_out=../backend/models *.proto
-../frontend/node_modules/protobufjs/bin/pbjs -t static-module -w commonjs -o compiled.js *.proto
-../frontend/node_modules/protobufjs/bin/pbts -o ../frontend/proto-types.d.ts compiled.js
-rm compiled.js
+../frontend/node_modules/protobufjs/bin/pbjs -t static-module -w commonjs -o ../frontend/proto-types.js *.proto
+../frontend/node_modules/protobufjs/bin/pbts -o ../frontend/proto-types.d.ts ../frontend/proto-types.js
